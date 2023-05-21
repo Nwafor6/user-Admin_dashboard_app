8023c47c60d6fb752a675123ccf7f7c910e0c55c
Nwafor6 <nwaforglory6@gmail.com>
2023-05-21 17:40:29 +0100

    setting up for versel cloud hopsting

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for project project.
+# """
+# WSGI config for project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+# """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+# application = get_wsgi_application()
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 application = get_wsgi_application()
+app = application
