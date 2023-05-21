08cac1b362cf6245efa855c432e48ed3c42688ad
Nwafor6 <nwaforglory6@gmail.com>
2023-05-21 19:51:34 +0100

    pusihing for versel hosting

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,21 +1,16 @@
-# """
-# WSGI config for project project.
+"""
+WSGI config for vercel_app project.
 
-# It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
-# For more information on this file, see
-# https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-# """
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-# application = get_wsgi_application()
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-application = get_wsgi_application()
-app = application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = get_wsgi_application()
