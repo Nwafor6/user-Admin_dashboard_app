738c1de37cc381c3ba59aad6bbbcd62722451aea
Nwafor6 <nwaforglory6@gmail.com>
2023-05-21 20:05:34 +0100

    fixing static file error of versel hosting

--- project/settings.py
+++ project/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL='/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join( BASE_DIR/'media/')
